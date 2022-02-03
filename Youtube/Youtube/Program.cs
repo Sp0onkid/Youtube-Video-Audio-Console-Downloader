6486e38493da7a490ea4ac66ff843821384d6d93
Sp0onkid <88853021+Sp0onkid@users.noreply.github.com>
2022-02-03 15:50:31 +0100

    Add files via upload

---
+++ Youtube/Youtube/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Text.RegularExpressions;
+using VideoLibrary;
+namespace Youtube
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            DisplayIntro();
+            VideoInfo();
+
+        }
+
+        public static async Task VideoInfo()
+        {
+
+            Console.WriteLine("Youtube Video Link:");
+            string Link = Console.ReadLine();
+            var youTube = YouTube.Default;
+            var video = youTube.GetVideo(Link);
+            string title = video.Title;
+            
+            Console.WriteLine("=========================================");
+            Console.WriteLine("=         1)DownloadAudio               =");
+            Console.WriteLine("=         2)DownloadVideo               =");
+            Console.WriteLine("=========================================");
+
+            string choice = Console.ReadLine();
+            if (Convert.ToInt32(choice) == 1)
+            {
+                string fullname = title + ".mp3";
+                byte[] bytes = video.GetBytes();
+                var stream = video.Stream();
+                File.WriteAllBytes(@"C:\Users\MY-PC\Desktop\music" + fullname, bytes);
+            }
+            if(Convert.ToInt32(choice) == 2)
+            {
+                string fullname = title + ".mp4";
+                byte[] bytes = video.GetBytes();
+                var stream = video.Stream();
+                File.WriteAllBytes(@"C:\Users\MY-PC\Desktop\music" + fullname, bytes);
+            }
+
+
+        }
+        public static void DisplayIntro()
+        {
+
+            Console.WriteLine("=========================================");
+            Console.WriteLine("=         Youtube Video Downloader      =");
+            Console.WriteLine("=             Spoonkid                  =");
+            Console.WriteLine("=========================================");
+        }
+
+    }
+}
